90c4948693a68335ea1b1d1e86fb7c0105e30eeb
donovics <kerchnerdani@gamil.com>
2025-09-07 13:10:51 +0200

    Beléptető rendszer

--- src/main/java/org/management/controllers/HomeController.java
+++ src/main/java/org/management/controllers/HomeController.java
@@ -1,12 +1,20 @@
 package org.management.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class HomeController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private VBox vBox;
     @FXML
     private javafx.scene.control.Button exitButton;
 
@@ -14,9 +22,17 @@ public class HomeController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+    /*@FXML
+    protected void onSingInButtonClick() throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/org/management/singInView.fxml"));
+        Stage stage = (Stage) singInButton.getScene().getWindow();
+        stage.setScene(new Scene(root, 600, 600));
+        stage.show();
+    }*/
     @FXML
-    protected void onSingInButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onSingInButtonClick() throws Exception {
+        Parent newContent = FXMLLoader.load(getClass().getResource("/org/management/singInView.fxml"));
+        vBox.getChildren().setAll(newContent);
     }
     @FXML
     protected void onExitButtonClick() {
--- src/main/java/org/management/controllers/SingInController.java
+++ src/main/java/org/management/controllers/SingInController.java
@@ -1,11 +1,53 @@
 package org.management.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class SingInController {
     @FXML
     private Label singInText;
+    @FXML
+    private VBox vBox;
+    @FXML
+    private TextField userName;
+    @FXML
+    private TextField password;
+
+    @FXML
+    protected void onSingInButtonClick() throws Exception{
+        FileReader fileReader = new FileReader("C:/repos/Management/src/main/resources/Users.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        boolean found = false;
+        String line;
+        while((line = bufferedReader.readLine()) != null){
+            if (line.split(" ")[0].equals(userName.getText())) {
+                found = true;
+                if (line.split(" ")[1].equals(password.getText())){
+                    singInText.setText("Sikeres belépés!");
+                }
+                else{
+                    singInText.setText("Hibás Jelszó");
+                }
+            }
+        }
+        if (!found){
+            singInText.setText("Nincs ilyen felhasználó név");
+        }
+        bufferedReader.close();
+        fileReader.close();
+    }
+    @FXML
+    protected void onHomeButtonClick() throws Exception {
+        Parent newContent = FXMLLoader.load(getClass().getResource("/org/management/homeView.fxml"));
+        vBox.getChildren().setAll(newContent);
+    }
 
 }
